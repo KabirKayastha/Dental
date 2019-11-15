4ad917b17d7d8f7f5f656fde17938170532da921
Kabir Kayastha <kayas.kabie@gmail.com>
2019-11-15 23:01:48 +0545

    added testimonials

--- js/customOwlCarosel.js
+++ js/customOwlCarosel.js
@@ -24,3 +24,62 @@ $(".banner .carousel-next").click(function() {
 $(".banner .carousel-prev").click(function() {
   bannerCarousel.trigger("prev.owl.carousel", [1000]);
 });
+
+var doctorsCarousel = $(".doctors .owl-carousel");
+doctorsCarousel.owlCarousel({
+  loop: true,
+  nav: false,
+  dots: false,
+  autoWidth: false,
+  autoplay: false,
+  autoplayTimeout: 5000,
+  smartSpeed: 1000,
+  fluidSpeed: 1000,
+  //   autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    300: {
+      items: 2
+    },
+    1000: {
+      items: 3
+    },
+    1200: {
+      items: 4
+    }
+  }
+});
+
+$(".doctors .carousel-next").click(function() {
+  // With optional speed parameter
+  // Parameters has to be in square bracket '[]'
+  doctorsCarousel.trigger("next.owl.carousel", [1000]);
+});
+$(".doctors .carousel-prev").click(function() {
+  doctorsCarousel.trigger("prev.owl.carousel", [1000]);
+});
+
+var testimonialsCarousel = $(".testimonials .owl-carousel");
+testimonialsCarousel.owlCarousel({
+  loop: true,
+  nav: false,
+  dots: false,
+  animateIn: "slideInRight",
+  animateOut: "fadeOutLeft",
+  autoWidth: false,
+  smartSpeed: 1000,
+  fluidSpeed: 1000,
+  responsive: {
+    0: {
+      items: 1
+    }
+  }
+});
+
+$(".testimonials .owl-dot").click(function() {
+  testimonialsCarousel.trigger("to.owl.carousel", [$(this).index()]);
+  $(".owl-dot").removeClass("active");
+  $(this).addClass("active");
+});
